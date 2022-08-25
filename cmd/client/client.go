2db0d887eebc5808df57802562c07c479b23704c
leonardodelira <leonardo.lira@hotmail.com.br>
2022-08-25 12:10:10 -0300

    feat: example stream grpc

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/leonardodelira/go-grpc/pb"
@@ -17,6 +18,11 @@ func main() {
 	defer conn.Close()
 
 	client := pb.NewUserServiceClient(conn)
+	AddUser(client)
+	AddUserVerbose(client)
+}
+
+func AddUser(client pb.UserServiceClient) {
 	user, err := client.AddUser(context.Background(), &pb.User{
 		Id:    "999",
 		Name:  "Joy",
@@ -26,5 +32,23 @@ func main() {
 		fmt.Errorf("Something was wrong", err)
 		return
 	}
-	fmt.Print(user)
+	fmt.Println(user)
+}
+
+func AddUserVerbose(client pb.UserServiceClient) {
+	response, err := client.AddUserVerbose(context.Background(), &pb.User{
+		Id:    "999",
+		Name:  "Joy",
+		Email: "joy@teste.com",
+	})
+	if err != nil {
+		fmt.Errorf("Error on AddUserVerbose stream", err)
+	}
+	for {
+		stream, err := response.Recv()
+		if err == io.EOF { //quando a stream finalizar esse "erro" é disparado
+			break
+		}
+		fmt.Println(stream)
+	}
 }
--- services/user-service.go
+++ services/user-service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"time"
 
 	"github.com/leonardodelira/go-grpc/pb"
 )
@@ -20,4 +21,29 @@ func (*userService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error)
 		Name:  req.GetName(),
 		Email: req.GetEmail(),
 	}, nil
+}
+
+func (*userService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+	stream.Send(&pb.UserResultStream{
+		Status: "Init",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Inserting",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "User inserted",
+		User: &pb.User{
+			Id: "9999",
+		},
+	})
+
+	return nil
 }
